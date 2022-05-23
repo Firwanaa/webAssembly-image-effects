2af7a48ba32dd72addbdfac2cc9a7334c006442d
firwanaa <firwanaa@sheridancollege.ca>
2022-05-23 10:27:12 -0400

    covert colored image to grayscale image walaa!

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,31 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+use wasm_bindgen::prelude::*;
+use web_sys::console::log_1 as log;
+use base64::{encode, decode};
+use image::load_from_memory;
+use image::ImageOutputFormat::Png;
+#[wasm_bindgen]
+pub fn grayscale(encoded_file: &str) -> String{
+    log(&"Grayscle called!".into());
+
+    let base64_to_vector = decode(encoded_file).unwrap();
+
+    log(&"Image Decoded!".into());
+
+    let mut img = load_from_memory(&base64_to_vector).unwrap();
+    log(&"Image Loaded".into());
+
+    img = img.grayscale();
+    log(&"Grayscle effect applied".into());
+
+    let mut buffer = vec![];
+    img.write_to(&mut buffer, Png).unwrap();
+    log(&"New Image Writter!".into());
+
+    let encoded_img = encode(&buffer);
+    let data_url = format!(
+        "data:image/png;base64,{}",
+        encoded_img
+    );
+
+    data_url
 }
